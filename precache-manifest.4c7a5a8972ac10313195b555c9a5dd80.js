8539c410f4a28b662c512dc9978576d6c081f6bb
Maxim Predko <maxpredko@gmail.com>
2022-02-01 17:35:45 +0300

    Updates

---
+++ precache-manifest.4c7a5a8972ac10313195b555c9a5dd80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38583372474b2e430cf5b09c0638bec",
+    "url": "/Homework/index.html"
+  },
+  {
+    "revision": "a34e712eb412ff68811d",
+    "url": "/Homework/static/css/main.4d09d8c9.chunk.css"
+  },
+  {
+    "revision": "73bb2c98156fb4c2f0ab",
+    "url": "/Homework/static/js/2.43ed494c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Homework/static/js/2.43ed494c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a34e712eb412ff68811d",
+    "url": "/Homework/static/js/main.660f373a.chunk.js"
+  },
+  {
+    "revision": "094ff21f2c380933c05d",
+    "url": "/Homework/static/js/runtime-main.4ab2aa2b.js"
+  }
+]);
