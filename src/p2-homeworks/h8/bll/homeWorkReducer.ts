b5a7d64b3ccc3dbc12b595bbf0a87579afa112de
Maxim Predko <maxpredko@gmail.com>
2022-04-25 12:33:16 +0300

    feat: done HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,57 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
+import { PeoplesType, UserType } from "../HW8";
+
+// ===== ACTIONS =====
+export const SORT_UP = "SORT-UP";
+export const SORT_DOWN = "SORT-DOWN";
+export const CHECK_AGE = "CHECK-AGE";
+
+export type ActionsType = SortUpACType | SortDownACType | CheckAgeACType;
+
+type SortUpACType = {
+  type: typeof SORT_UP;
+};
+
+type SortDownACType = {
+  type: typeof SORT_DOWN;
+};
+
+type CheckAgeACType = {
+  type: typeof CHECK_AGE;
+  payload: number;
+};
+
+export const sortUpAC = (): SortUpACType => ({ type: SORT_UP });
+export const sortDownAC = (): SortDownACType => ({ type: SORT_DOWN });
+export const checkAgeAC = (sortAge: number): CheckAgeACType => ({
+  type: CHECK_AGE,
+  payload: sortAge,
+});
+
+export const homeWorkReducer = (
+  state: UserType,
+  action: ActionsType
+): UserType => {
+  switch (action.type) {
+    case SORT_UP: {
+      return [
+        ...state.sort(
+          (a: PeoplesType, b: PeoplesType) =>
+            a.name.charCodeAt(0) - b.name.charCodeAt(0)
+        ),
+      ];
     }
-}
+    case SORT_DOWN: {
+      return [
+        ...state.sort(
+          (a: PeoplesType, b: PeoplesType) =>
+            b.name.charCodeAt(0) - a.name.charCodeAt(0)
+        ),
+      ];
+    }
+    case CHECK_AGE: {
+      return [...state.filter((el) => el.age >= action.payload)];
+    }
+    default:
+      return state;
+  }
+};
--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,32 +1,58 @@
-import React from 'react'
-import {homeWorkReducer} from '../homeWorkReducer'
+import React from "react";
+import { UserType } from "../../../h8/HW8";
+import {
+  homeWorkReducer,
+  ActionsType,
+  SORT_UP,
+  SORT_DOWN,
+  CHECK_AGE,
+} from "../homeWorkReducer";
 
-let initialState: any[] // need to fix any
+let initialState: UserType; // need to fix any
 
 beforeEach(() => {
-    initialState = [
-        {_id: 0, name: 'Кот', age: 3},
-        {_id: 1, name: 'Александр', age: 66},
-        {_id: 2, name: 'Коля', age: 16},
-        {_id: 3, name: 'Виктор', age: 44},
-        {_id: 4, name: 'Дмитрий', age: 40},
-        {_id: 5, name: 'Ирина', age: 55},
-    ]
-})
+  initialState = [
+    { _id: 0, name: "Кот", age: 3 },
+    { _id: 1, name: "Александр", age: 66 },
+    { _id: 2, name: "Коля", age: 16 },
+    { _id: 3, name: "Виктор", age: 44 },
+    { _id: 4, name: "Дмитрий", age: 40 },
+    { _id: 5, name: "Ирина", age: 55 },
+  ];
+});
 
-test('sort name up', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
+test("sort name up", () => {
+  const newState = homeWorkReducer(initialState, {
+    type: SORT_UP,
+  });
 
-    console.log(newState)
-    // expect(...).toBe(...)
-})
-test('sort name down', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
+  console.log(newState);
+  expect(newState[0].name).toBe("Александр");
+  expect(newState[1].name).toBe("Виктор");
+  expect(newState[2].name).toBe("Дмитрий");
+  expect(newState[3].name).toBe("Ирина");
+});
 
+test("sort name down", () => {
+  const newState = homeWorkReducer(initialState, {
+    type: SORT_DOWN,
+  });
 
-})
-test('check age 18', () => {
-    const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
+  console.log(newState);
+  expect(newState[5].name).toBe("Александр");
+  expect(newState[1].name).toBe("Коля");
+  expect(newState[2].name).toBe("Ирина");
+  expect(newState[3].name).toBe("Дмитрий");
+});
 
+test("check age 18", () => {
+  const newState = homeWorkReducer(initialState, {
+    type: CHECK_AGE,
+    payload: 18,
+  });
 
-})
+  console.log(newState);
+  expect(newState.length).toBe(4);
+  expect(newState[0].name).toBe("Александр");
+  expect(newState[0].age).toBe(66);
+});
