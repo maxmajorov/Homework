61c06ae87aa68f7e37e1cd473ccbcce26d054360
Maxim Predko <maxpredko@gmail.com>
2022-05-13 22:21:07 +0300

    feat: done HW11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,39 +1,49 @@
-import React, {useState} from 'react'
-import SuperRange from './common/c7-SuperRange/SuperRange'
-import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import React, { useState } from "react";
+import SuperRange from "./common/c7-SuperRange/SuperRange";
+import SuperDoubleRange from "./common/c8-SuperDoubleRange/SuperDoubleRange";
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(100);
 
-    return (
-        <div>
-            <hr/>
-            homeworks 11
+  const arrValues: any = [value1, value2];
+  const valuesHandler = (value: number | number[]) => {
+    setValue1(Array.isArray(value) ? value.reduce((a: number) => a) : value);
+    setValue2(
+      Array.isArray(value)
+        ? value.reduce((a: number, b: number) => 0 + b)
+        : value
+    );
+  };
 
-            {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
-            </div>
-
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <hr />
+      homeworks 11
+      <div>
+        <span>{value1}</span>
+        <SuperRange onChangeRange={setValue1} value={value1} />
+      </div>
+      <div>
+        <span>{value1}</span>
+        &nbsp;
+        <SuperDoubleRange onChangeRange={valuesHandler} value={arrValues} />
+        &nbsp;
+        <span>{value2}</span>
+      </div>
+      <hr />
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperRange/>*/}
+      {/*<AlternativeSuperDoubleRange/>*/}
+      <hr />
+    </div>
+  );
 }
 
-export default HW11
+export default HW11;
--- src/p2-homeworks/h11/common/c7-SuperRange/SuperRange.tsx
+++ src/p2-homeworks/h11/common/c7-SuperRange/SuperRange.tsx
@@ -1,43 +1,52 @@
-import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
-import s from './SuperRange.module.css'
+import React, {
+  ChangeEvent,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+} from "react";
+import s from "./SuperRange.module.css";
 
 // тип пропсов обычного инпута
-type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type DefaultInputPropsType = DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
 
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
-type SuperRangePropsType = DefaultInputPropsType & { // и + ещё пропсы которых нет в стандартном инпуте
-    onChangeRange?: (value: number) => void
+type SuperRangePropsType = DefaultInputPropsType & {
+  // и + ещё пропсы которых нет в стандартном инпуте
+  onChangeRange?: (value: number) => void;
+  value: number;
 };
 
-const SuperRange: React.FC<SuperRangePropsType> = (
-    {
-        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
-        onChange, onChangeRange,
-        className,
+const SuperRange: React.FC<SuperRangePropsType> = ({
+  type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
+  onChange,
+  value,
+  onChangeRange,
+  className,
 
-        ...restProps// все остальные пропсы попадут в объект restProps
-    }
-) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange && onChange(e) // сохраняем старую функциональность
+  ...restProps // все остальные пропсы попадут в объект restProps
+}) => {
+  const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange && onChange(e); // сохраняем старую функциональность
 
-        onChangeRange && onChangeRange(+e.currentTarget.value)
-    }
+    onChangeRange && onChangeRange(+e.currentTarget.value);
+  };
 
-    const finalRangeClassName = `${s.range} ${className ? className : ''}`
+  const finalRangeClassName = `${s.range} ${className ? className : ""}`;
 
-    return (
-        <>
-            <input
-                type={'range'}
-                onChange={onChangeCallback}
-                className={finalRangeClassName}
+  return (
+    <>
+      <input
+        type={"range"}
+        value={value}
+        onChange={onChangeCallback}
+        className={finalRangeClassName}
+        {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+      />
+    </>
+  );
+};
 
-                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
-            />
-        </>
-    )
-}
-
-export default SuperRange
+export default SuperRange;
