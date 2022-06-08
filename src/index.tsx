bcd2aca6211f9a5ad3987e75e4cb470e3c3f049c
Maxim Predko <maxpredko@gmail.com>
2022-06-08 11:12:48 +0300

    init friday project

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { HashRouter } from "react-router-dom";
 import "./index.css";
-import App from "./p1-main/m1-ui/u1-app/App";
-import { store } from "./p2-homeworks/h10/bll/store";
+import { App } from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <HashRouter>
@@ -16,7 +16,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
--- src/p2-homeworks/h10/loader.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { CirclesWithBar } from "react-loader-spinner";
-
-// import classes from "./Loader.module.css";
-
-export const LoaderVariant = () => {
-  return <CirclesWithBar color="blue" />;
-};
